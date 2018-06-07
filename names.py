a5dda98a1f539b1cf371a47785e6b6ee66c937a5
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-06-06 22:09:39 -0700

    Completed assignment Names

---
+++ names.py
@@ -0,0 +1,45 @@
+# Assignment: Names
+# Karen Clark
+# 2018-06-06
+
+# Assignment: Names
+# Write the following function.
+
+# Part I
+# Given the following list, create a program that outputs "first_name last_name"
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def print_list(students):
+    for i in students:
+        print i['first_name'], i['last_name']
+    
+# Part II
+# Now, given the following dictionary, Create a program that prints the following format (including number of characters in each combined name)
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+def print_dict(a_dict):
+    for i in a_dict:
+        counter = 0
+        print i
+        for person in a_dict[i]:
+            counter += 1
+            first_name = person['first_name'].upper()
+            last_name = person['last_name'].upper()
+            length = len(first_name) + len(last_name)
+            print "{} - {} {} - {}".format(counter, first_name, last_name, length)

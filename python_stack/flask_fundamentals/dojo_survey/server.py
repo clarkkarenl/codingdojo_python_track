50facd3c0dd1035f45a27e780e67f3258592c14c
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-06-11 23:45:29 -0700

    Completed Dojo Survey assignment

---
+++ python_stack/flask_fundamentals/dojo_survey/server.py
@@ -0,0 +1,34 @@
+# Assignment: Dojo Survey
+# Karen Clark
+# 2018-06-11
+
+# Assignment: Dojo Survey
+# Build a flask application that accepts a form submission, 
+# redirects, and presents the submitted data on a results page.
+# Hint: Although we've told you never to render from a post 
+# route, you'll need to do so for this assignment. We'll show 
+# you tools to avoid doing so soon.
+
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def gather_data():
+    if request.method == 'POST':
+        # name = request.form.get['name']
+        # dojo_loc = request.form.get['dojo_loc']
+        # fav_lang = request.form.get['fav_lang']
+        # comment = request.form.get['comment']
+        result = request.form
+        return render_template('result.html', result = result)
+    else:
+        return render_template('result.html')
+
+
+app.run(debug=True)

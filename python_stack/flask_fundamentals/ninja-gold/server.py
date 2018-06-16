b06c838ff942b7cdfe36ddcc60944aac9eb6ea74
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-06-16 16:22:27 -0700

    Added feature to handle zero or negative balance

--- python_stack/flask_fundamentals/ninja-gold/server.py
+++ python_stack/flask_fundamentals/ninja-gold/server.py
@@ -1,15 +1,8 @@
-# Assignment: Counter
+# Assignment: Ninja Gold
 # Karen Clark
 # 2018-06-16
-#
-# Assignment: Ninja Game
-# Create a site that when a user loads it creates a random number between
-# 1-100 and stores the number in session. Allow the user to guess at the
-# number and tell them when they are too high or too low. If they guess
-# the correct number tell them and offer to play again.
 
-import datetime
-import random
+import datetime, random
 from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
@@ -17,28 +10,36 @@ app.secret_key = 'ThisIsSecret'
 
 @app.route('/')
 def index():
+    session['balance'] = 0
+    session['activities'] = ""
     return render_template('index.html')
-        
-@app.route('/process', methods=['GET', 'POST'])
-def process():
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    session.pop('balance', None)
+    session.pop('register', None)
+    return redirect('/')
+
+@app.route('/process_money', methods=['POST'])
+def process_money():
     # TODO create a new function that does the heavy lifting for each target
     if request.method == 'POST':
-        target = request.form.get('shop')
+        target = request.form['building']
         if target == 'farm':
             new_gold = random.randrange(10, 21)
             session['balance'] = int(session['balance']) + new_gold
             timestamp = datetime.datetime.now()
-            session['register'] += "Earned " + str(new_gold) + " gold from the farm! (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
+            session['activities'] += "Earned " + str(new_gold) + " gold from the farm! (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
         elif target == 'cave':
             new_gold = random.randrange(5, 11)
             session['balance'] = int(session['balance']) + new_gold
             timestamp = datetime.datetime.now()
-            session['register'] += "Earned " + str(new_gold) + " gold from the cave! (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
+            session['activities'] += "Earned " + str(new_gold) + " gold from the cave! (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
         elif target == 'house':
-            new_gold = random.randrange(5, 11)
+            new_gold = random.randrange(2, 6)
             session['balance'] = int(session['balance']) + new_gold
             timestamp = datetime.datetime.now()
-            session['register'] += "Earned " + str(new_gold) + " gold from the house! (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
+            session['activities'] += "Earned " + str(new_gold) + " gold from the house! (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
         elif target == 'casino':
             new_gold = random.randrange(0, 51)
             timestamp = datetime.datetime.now()
@@ -49,14 +50,17 @@ def process():
                 # lost gold
                 if outcome == 1:
                     session['balance'] = int(session['balance']) - new_gold
-                    session['register'] += "Entered a casino and lost " + str(new_gold) + " gold...Ouch. (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
+                    session['activities'] += "Entered a casino and lost " + str(new_gold) + " gold...Ouch. (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
+                    if session['balance'] <= 0:
+                        session['balance'] = 50
+                        session['activities'] += "You went bust! Thankfully, your rich uncle paid the debt and gave you 50 gold to keep going. (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
                 # won gold
                 else:
                     session['balance'] = int(session['balance']) + new_gold
-                    session['register'] += "Entered a casino and won " + str(new_gold) + " gold! (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
+                    session['activities'] += "Entered a casino and won " + str(new_gold) + " gold! (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
             # else new_gold was 0
             else:
-                session['register'] += "Entered a casino but did not win or lose gold. (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
+                session['activities'] += "Entered a casino but did not win or lose gold. (" + timestamp.strftime("%Y/%m/%d %I:%M %p") + ")<br/>"
         return render_template('index.html')
     
 app.run(debug=True)

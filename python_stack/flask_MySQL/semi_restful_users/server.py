e09805dc44e91ca2f208308cdfe5e41c1ddb225e
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-07-04 16:00:39 -0700

    More functionality added

--- python_stack/flask_MySQL/semi_restful_users/server.py
+++ python_stack/flask_MySQL/semi_restful_users/server.py
@@ -3,24 +3,32 @@ from mysqlconnection import MySQLConnector
 
 app = Flask(__name__)
 mysql = MySQLConnector(app,'friends')
+app.secret_key = 'SekritzRSekrit'
 
-app.secret_key = 'AnotherBrickIn'
-
-# DONE - GET request to /users - calls the index method to display all the users.
+# GET request to /users - calls the index method to display all the users.
 @app.route('/users', methods=['GET'])
 def index(): 
     get_users_query = "SELECT `users`.`id` as `user_id`, CONCAT(`users`.`first_name`,\" \", `users`.`last_name`) as `user_name`,  `users`.`email` as `email`, DATE_FORMAT(`users`.`created_at`, \"%M %D, %Y\") as `created_at` FROM `users` ORDER BY `users`.`created_at` ASC;"
     user_list = mysql.query_db(get_users_query)
     return render_template('users.html', users = user_list)
 
-# DONE - GET request to /users/new - calls the new method to display a form allowing users to create a new user.
-@app.route('/users/new', methods=['GET'])
-def new():
-    return render_template('new.html')
 
 # GET request /users/<id>/edit - calls the edit method to display a form allowing users to edit an existing user with the given id. 
+@app.route('/users/<id>/edit', methods=['GET'])
+def edit(id):
+    
+    get_edit_user_query = "SELECT `users`.`id` as `user_id`,`users`.`first_name` as `first_name`, `users`.`last_name` as `last_name`, `users`.`email` as `email` FROM `users` WHERE `users`.`id` = :field_one;"
 
-# DONE - GET /users/<id> - calls the show method to display the info for a particular user with given id.
+    get_edit_user_data = {'field_one' : id}
+
+    user_to_edit = mysql.query_db(get_edit_user_query, get_edit_user_data)
+    return render_template('edit.html', user=user_to_edit[0])
+
+
+# POST /users/<id> - calls the update method to process the submitted form sent from /users/<id>/edit. Have this redirect to /users/<id> once updated.
+
+
+# GET /users/<id> - calls the show method to display the info for a particular user with given id.
 @app.route('/users/<id>', methods=['GET'])
 def show(id):
     get_single_user_query = "SELECT `users`.`id` as `user_id`, CONCAT(`users`.`first_name`,\" \", `users`.`last_name`) as `user_name`,  `users`.`email` as `email`, DATE_FORMAT(`users`.`created_at`, \"%M %D, %Y\") as `created_at` FROM `users` WHERE `users`.`id` = :field_one;"
@@ -29,6 +37,13 @@ def show(id):
 
     user = mysql.query_db(get_single_user_query, get_single_user_data)
     return render_template('show.html', user=user[0])
+
+
+# GET request to /users/new - calls the new method to display a form allowing users to create a new user.
+@app.route('/users/new', methods=['GET'])
+def new():
+    return render_template('new.html')
+
 
 # POST to /users/create - calls the create method to insert a new user record into our database. This POST should be sent from the form on the page /users/new. Have this redirect to /users/<id> once created.
 @app.route('/users/create', methods=['POST'])
@@ -48,20 +63,15 @@ def create():
     user_id = create_user_result
     return redirect('/users/' + str(user_id))
 
-# DONE - GET /users/<id>/destroy - calls the destroy method to remove a particular user with the given id. Have this redirect back to /users once deleted.
+
+# GET /users/<id>/destroy - calls the destroy method to remove a particular user with the given id. Have this redirect back to /users once deleted.
 @app.route('/users/<id>/destroy', methods=['POST'])
-def destroy(user_id):
+def destroy(id):
     delete_user_query = "DELETE FROM `users` WHERE `users`.`id` = :field_one;"
-    delete_user_data = {'field_one' : user_id}
+    delete_user_data = {'field_one' : id}
 
-    try:
-        user_delete = mysql.query_db(delete_user_query, delete_user_data)
-        return redirect('/users')
-    except Exception as e:
-        flash("Nope, sorry! Error: " + str(e))
-        return redirect('/users')
-
-# POST /users/<id> - calls the update method to process the submitted form sent from /users/<id>/edit. Have this redirect to /users/<id> once updated.
+    user_delete = mysql.query_db(delete_user_query, delete_user_data)
+    return redirect('/users')
 
 
 if __name__ == '__main__':

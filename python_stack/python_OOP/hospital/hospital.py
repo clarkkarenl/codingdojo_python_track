28a92839b28e13c33abb44a4a81857f9b5d91c11
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-07-06 16:03:49 -0700

    Completed assignment Hospital.

--- python_stack/python_OOP/hospital/hospital.py
+++ python_stack/python_OOP/hospital/hospital.py
@@ -2,6 +2,15 @@
 # Karen Clark
 # 2018-07-06
 
+# ================================================================
+# NOTE:
+# I struggled with this one quite a bit. I finally gave up and...
+# 1. watched Minh's code review for Hospital
+# 2. looked at the solution
+# 3. realized I needed to invoke the Hospital functions 
+#  differently than I was
+# ================================================================
+
 # You're going to build a hospital with patients in it! Create a hospital class.
 # Before looking at the requirements below, think about the potential characteristics of each patient and hospital. How would you design each?
 # Patient:
@@ -11,13 +20,13 @@
 # * Allergies
 # * Bed number: should be none by default
 class Patient(object):
-    patient_count = 0
+    PATIENT_COUNT = 0
     def __init__(self, name, allergies):
-        self.id = Patient.patient_count
         self.name = name
         self.allergies = allergies
+        self.id = Patient.PATIENT_COUNT
         self.bed_num = None
-        Patient.patient_count += 1
+        Patient.PATIENT_COUNT += 1
 
     def patient_info(self):
         print "Patient id:", self.id
@@ -48,7 +57,7 @@ class Hospital(object):
 
 # * Admit: add a patient to the list of patients. Assign the patient a bed number. If the length of the list is >= the capacity do not admit the patient. Return a message either confirming that admission is complete or saying the hospital is full.
     def admit(self, patient):
-        if len(self.patients) < self.capacity:
+        if len(self.patients) <= self.capacity:
             self.patients.append(patient) 
             for i in range(0, len(self.beds)):
                 if self.beds[i]['available']:
@@ -57,9 +66,21 @@ class Hospital(object):
                     break
             print "Patient {} admitted to bed {}".format(patient.id, patient.bed_num)
         else:
-            print "Hospital is at maxiumum capacity. Please try another hospital."
+            print "Hospital", self.name, " is full."
 
-# * Discharge: look up and remove a patient from the list of patients. Change bed number for that patient back to none.
+
+    def discharge(self, patient_id):
+        for patient in self.patients:
+            if patient.id == patient_id:
+                for bed in self.beds:
+                    if bed['bed_id'] == patient.bed_num:
+                        bed['available'] = True
+                        break
+
+                self.patients.remove(patient)
+                print "Patient #{} sucessfully discharged.  Bed #{} now available".format(patient.id, patient.bed_num)
+
+
     def hospital_info(self):
         print "Hospital name:", self.name
         print "Patient capacity:", self.capacity
@@ -78,5 +99,7 @@ h1 = Hospital("St. John", 3)
 h1.admit(p1)
 h1.admit(p4)
 h1.admit(p2)
+print "discharging p2 (p2.id = 2)"
+h1.discharge(p2.id)
 h1.admit(p3)
 h1.hospital_info()

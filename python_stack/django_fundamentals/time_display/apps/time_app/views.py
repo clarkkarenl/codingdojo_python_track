bd246b170fd6e2afc57831bab6d33597fc0f8059
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-07-07 15:52:34 -0700

    Checkpoint commit Time Display assignment

--- python_stack/django_fundamentals/time_display/apps/time_app/views.py
+++ python_stack/django_fundamentals/time_display/apps/time_app/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, redirect, render
+from time import gmtime, strftime
 
 # Create your views here.
+def index(request):
+    # now = datetime.datetime.now()
+    context = {
+        "date_output": strftime("%b %d, %Y", gmtime()),
+        "time_output": strftime("%H:%M %p", gmtime())
+    }
+    return render(request,'time_app/index.html', context)

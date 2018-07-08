9040521c421343362f96987578cf70466190d107
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-07-08 16:51:44 -0700

    Completed assignment Random Word

--- python_stack/django_fundamentals/random_word/apps/random_word_app/views.py
+++ python_stack/django_fundamentals/random_word/apps/random_word_app/views.py
@@ -6,30 +6,28 @@ from django.utils.crypto import get_random_string
 
 # Create your views here.
 def index(request):
-    # if it's the users first visit, won't have this var,
-    # so set it
-    if 'counter' not in request.session:
-        request.session['counter'] = 1
-
-    count_var = request.session['counter']
-
-    context = {
-        "count_var" : count_var,
-        "word" : get_random_string(14)
-    }
+    context = make_word(request)
     return render(request,'random_word_app/index.html', context)
+
 
 def generate(request):
-    count_var = request.session.get('counter')
-    count_var += 1
-    request.session['counter'] += 1
-
-    context = {
-        "count_var" : count_var,
-        "word" : get_random_string(14)
-    }
+    context = make_word(request)
     return render(request,'random_word_app/index.html', context)
+
 
 def reset(request):
     del request.session['counter']
     return redirect(index)
+
+
+def make_word(request):
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
+
+    request.session['counter'] += 1
+
+    context = {
+        "count_var" : request.session['counter'],
+        "word" : get_random_string(14)
+    }
+    return context

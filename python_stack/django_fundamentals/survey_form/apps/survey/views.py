c4d533ce3260bd0ba77672a5a4cee385aa16ab9c
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-07-09 23:04:52 -0700

    Checkpoint commit Survey Form assignment

--- python_stack/django_fundamentals/survey_form/apps/survey/views.py
+++ python_stack/django_fundamentals/survey_form/apps/survey/views.py
@@ -1,8 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib import messages
 from django.shortcuts import HttpResponse, redirect, render
 
 # Create your views here.
 def index(request):
+    if 'name' not in request.session:
+        name = ""
+    if 'dojo_loc' not in request.session:
+        dojo_loc = ""
+    if 'fav_lang' not in request.session:
+        fav_lang = ""
+    if 'comments' not in request.session:
+        comments = ""
     return render(request, "survey/index.html")
+
+def process(request):
+    if request.method == 'POST':
+        if 'name' not in request.session:
+            name = request.session.get('name')
+        if 'dojo_loc' not in request.session:
+            dojo_loc = request.session.get('dojo_loc')
+        if 'fav_lang' not in request.session:
+            fav_lang = request.session.get('fav_lang')
+        if 'comments' not in request.session:
+            comments = request.session.get('comments')
+
+        if not name or not dojo_loc or not fav_lang:
+            messages.error(request, "Please provide values for all fields")
+        
+        return redirect("result.html")
+        
+
+def result(request):
+    return redirect("/")

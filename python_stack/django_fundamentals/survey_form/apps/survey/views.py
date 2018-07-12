c616536653d41e5426100521c60c718339080212
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-07-11 18:37:52 -0700

    Updates from code review. Completed assignment.

--- python_stack/django_fundamentals/survey_form/apps/survey/views.py
+++ python_stack/django_fundamentals/survey_form/apps/survey/views.py
@@ -6,26 +6,33 @@ from django.shortcuts import HttpResponse, redirect, render
 
 # Create your views here.
 def index(request):
-    if 'name' not in request.session:
-        name = ""
-    if 'dojo_loc' not in request.session:
-        dojo_loc = ""
-    if 'fav_lang' not in request.session:
-        fav_lang = ""
-    if 'comment_box' not in request.session:
-        comment_box = ""
+    if 'name' not in request.session['data']['name']:
+        request.session['data']['name'] = ""
+    if 'dojo_loc' not in request.session['data']:
+        request.session['data']['dojo_loc'] = ""
+    if 'fav_lang' not in request.session['data']:
+        request.session['data']['fav_lang'] = ""
+    if 'comment_box' not in request.session['data']:
+        request.session['data']['comment_box'] = ""
     return render(request, "survey/index.html")
 
 
 def process(request):
-        "comment_box" : comment_box
-    }
+    if request.method == 'POST':    
+        name = request.POST['name']
+        dojo_loc = request.POST['dojo_loc']
+        fav_lang = request.POST['fav_lang']
+        comment_box = request.POST['comment_box']
 
-    if not name or not dojo_loc or not fav_lang:
-        messages.error(request, "Please provide values for all fields")
-    
-    return redirect("survey/result", context)
+        request.session["data"]  = {
+            "name" : name,
+            "dojo_loc" : dojo_loc,
+            "fav_lang" : fav_lang,
+            "comment_box" : comment_box
+        }
 
+        return redirect("/survey/result")
+ 	 
 
 def result(request):
     return render(request, "survey/result.html")

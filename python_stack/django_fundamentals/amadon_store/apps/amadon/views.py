29dd9ab01944cdf993071219450d51395f056653
Karen Clark <clarkkarenl@users.noreply.github.com>
2018-07-13 23:55:39 -0700

    More progress on Amadon

--- python_stack/django_fundamentals/amadon_store/apps/amadon/views.py
+++ python_stack/django_fundamentals/amadon_store/apps/amadon/views.py
@@ -11,4 +11,6 @@ def index(request):
 def checkout(request):
     # T-shirt = 1015 = $19.99
     # Sweater = 1016 = $29.99
+    # Cup = 1017 = $4.99
+    # Book = 1018 = $49.99
     return render(request, "amadon/checkout.html")
